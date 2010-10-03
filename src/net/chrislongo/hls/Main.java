6afc711db14252c7f2f0a40b9ac0d2f966bc20f7
Chris Longo <chris.longo@gmail.com>
2010-10-03 10:39:45 -0400

    GPL

--- src/net/chrislongo/hls/Main.java
+++ src/net/chrislongo/hls/Main.java
@@ -1,10 +1,26 @@
+/*
+ * Copyright (c) 2010 Christopher A Longo
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
 package net.chrislongo.hls;
 
 import java.io.*;
 
 /**
- * Created by IntelliJ IDEA.
- * User: clongo
+ * User: chris
  * Date: Oct 1, 2010
  * Time: 12:22:32 PM
  */
